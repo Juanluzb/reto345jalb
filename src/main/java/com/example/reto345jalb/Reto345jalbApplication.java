a818099a33cd0ecfdffaa363feaf065733e6ec23
JUAN ALEXANDER LUZ BEJARANO <juanluzb@colsubsidio.com>
2022-10-15 11:01:19 -0500

    primer commit

---
+++ src/main/java/com/example/reto345jalb/Reto345jalbApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto345jalb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto345jalbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto345jalbApplication.class, args);
+	}
+
+}
